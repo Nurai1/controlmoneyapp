0df60049242374e506f0a9c2d11e69a005876952
Ainur <sayapow.ainur@mail.ru>
2020-02-17 15:58:55 +0300

    added some tests

--- src/components/ExpensesList.js
+++ src/components/ExpensesList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
-const ExpensesList = ({ expensesList }) => {
+export const ExpensesList = ({ expensesList }) => {
   if (expensesList !== undefined && expensesList.length !== 0) {
     return (
       <ul className="expensesList">
--- src/tests/actions.test.js
+++ src/tests/actions.test.js
@@ -2,6 +2,15 @@ import * as actions from '../actions';
 import * as types from '../constants';
 
 describe('actions', () => {
+  it('should create an action to add general sum', () => {
+    const value = 20000;
+    const expectedGeneralSum = {
+      type: types.ADD_GENERAL_SUM,
+      value,
+    }
+    expect(actions.addGeneralSum(value)).toEqual(expectedGeneralSum);
+  });
+
   it('should create an action to add expense', () => {
     const value = 200;
     const name = 'lunch';
@@ -11,5 +20,18 @@ describe('actions', () => {
       name,
     }
     expect(actions.addExpenses(value, name)).toEqual(expectedExpense);
+  });
+
+  it('should create an action to add purchase', () => {
+    const expenseId = 1;
+    const value = 200;
+    const name = 'lunch';
+    const expectedPurchase = {
+      type: types.ADD_PURCHASE,
+      expenseId,
+      value,
+      name,
+    }
+    expect(actions.addPurchase(expenseId, value, name)).toEqual(expectedPurchase);
   });
 });
